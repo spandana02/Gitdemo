071a4af4dd85ea16d42a09efd8d03afe8ac9d950
spandana02 <dasarispandana2@gmail.com>
2021-05-08 02:39:16 +0530

    first commit

---
+++ src/test/java/Testmethods/AutomateTestPDFs.java
@@ -0,0 +1,15 @@
+package Testmethods;
+
+import org.testng.annotations.Test;
+
+
+
+public class AutomateTestPDFs {
+
+	@Test(priority=0)
+public void testing1()
+{
+		System.out.println("HI");}
+
+}
+
