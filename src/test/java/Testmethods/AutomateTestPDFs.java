66283bbff711aa6ba8c79f9bda51306a551cc6d4
spandana02 <dasarispandana2@gmail.com>
2021-05-08 14:32:04 +0530

    updated commit from asian time zone guy

--- src/test/java/Testmethods/AutomateTestPDFs.java
+++ src/test/java/Testmethods/AutomateTestPDFs.java
@@ -2,14 +2,14 @@ package Testmethods;
 
 import org.testng.annotations.Test;
 
-
-
 public class AutomateTestPDFs {
 
-	@Test(priority=0)
-public void testing1()
-{
-		System.out.println("HI");}
+	@Test(priority = 0)
+	public void testing1() {
+		System.out.println("HI");
+		System.out.println("Hello");
+		System.out.println("Hello World");
+
+	}
 
 }
-
